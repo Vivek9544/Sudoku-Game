7308b21c6b44646be66ef568c278c8a882e5d187
Vivek Thatisetti <thatisettivivek@gmail.com>
2025-07-02 17:24:40 +0530

    Update Sudoku.h

--- src/Sudoku.h
+++ src/Sudoku.h
@@ -1,1 +1,25 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
 
+class Sudoku {
+    int board[9][9];
+    int solution[9][9];
+
+public:
+    Sudoku();
+    void generatePuzzle();
+    bool solve();
+    void printBoard();
+    void playGame();
+
+private:
+    bool isSafe(int row, int col, int num);
+    bool findEmpty(int &row, int &col);
+    void fillDiagonalBoxes();
+    void fillBox(int row, int col);
+    void removeDigits(int count);
+    bool solveBoard();
+    bool isComplete();
+};
+
+#endif
