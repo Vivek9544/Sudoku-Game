0a794fa5130b0ed7baacef9ec4528a8ad1b3c64c
Vivek Thatisetti <thatisettivivek@gmail.com>
2025-07-02 17:25:14 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,34 @@
+#include "Sudoku.h"
+#include <iostream>
+using namespace std;
 
+int main() {
+    Sudoku game;
+    int choice;
+
+    while (true) {
+        cout << "\nSudoku Game Menu:\n";
+        cout << "1. Play Game\n";
+        cout << "2. Exit\n";
+        cout << "Enter choice: ";
+
+        if (!(cin >> choice)) {
+            cin.clear(); cin.ignore(10000, '\n');
+            cout << "Invalid input.\n"; continue;
+        }
+
+        if (choice == 1) {
+            game.generatePuzzle();
+            game.playGame();
+            cout << "\nSolution:\n";
+            game.solve();
+            game.printBoard();
+        } else if (choice == 2) {
+            cout << "Exiting game.\n"; break;
+        } else {
+            cout << "Invalid choice. Try again.\n";
+        }
+    }
+
+    return 0;
+}
